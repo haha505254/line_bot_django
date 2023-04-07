0b1a83d909eaf6cae4603c171e58f8813b8a2847
Jack.lin <jack.lin@wsos.com.tw>
2023-04-07 22:54:01 +0800

    成功使用url訪問音檔

--- my_line_bot/line_bot_app/urls.py
+++ my_line_bot/line_bot_app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-
+from django.conf.urls.static import static
 from . import views
+from django.conf import settings
+
 
 urlpatterns = [
     path('callback/', views.callback, name='callback'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- my_line_bot/line_bot_app/views.py
+++ my_line_bot/line_bot_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage,AudioMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage,AudioMessage,AudioSendMessage
 import uuid
 import os
 import requests
@@ -12,13 +12,22 @@ from pydub import AudioSegment
 import io
 import requests
 import speech_recognition as sr
-
+from django.conf import settings
+import uuid
+import os
+import logging
+from django.core.files.storage import default_storage
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
 
 OPENAI_API_KEY = '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'
 line_bot_api = LineBotApi('ETKCbwm9fhQuQ2Tu3KjSo71tIc1jUdOF5Q3jdVWsN/EQ5G9x8gj2PPn8BZQc4Se7uwfVuzzQyqtYqRj+fZXFB/xMKDjKHpJUPko3w2+LoPNzcZxVEYooGacQR45+GJsUl4f8ZOy9GRLwFozzypH9VwdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('f48d4087c04e00b31b1b5264d1dcc2a5')
+
+
+# Ensure static directory exists
+if not os.path.exists(settings.STATIC_ROOT):
+    os.makedirs(settings.STATIC_ROOT)
 
 
 def speech_to_text(audio_file_path):
@@ -55,21 +64,24 @@ def callback(request):
 @handler.add(MessageEvent, message=AudioMessage)
 def handle_audio_message(event):
     message_content = line_bot_api.get_message_content(event.message.id)
+    # 在 handle_audio_message 函数中
     unique_filename = f"{uuid.uuid4()}.m4a"
 
-    with open(unique_filename, 'wb') as audio_file:
+    # 保存音频文件到 media 文件夹
+    audio_file_path = os.path.join(settings.MEDIA_ROOT, unique_filename)
+    with open(audio_file_path, 'wb') as audio_file:
         audio_file.write(message_content.content)
+
+    # 获取音频文件的 URL
+    audio_file_url = f"{settings.MEDIA_URL}{unique_filename}"
+
+
 
     logging.info("Received and saved audio message")
 
-    # Convert m4a to wav
-    m4a_audio = AudioSegment.from_file(unique_filename, "m4a")
-    wav_filename = unique_filename.replace('.m4a', '.wav')
-    m4a_audio.export(wav_filename, format="wav")
-
-    text = speech_to_text(wav_filename)
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=text))
-
+    print(audio_file_url)
+    # audio_send_message = AudioSendMessage(original_content_url=audio_url, duration=event.message.duration)
+    # line_bot_api.reply_message(event.reply_token, audio_send_message)
     # Clean up the files
     # os.remove(unique_filename)
     # os.remove(wav_filename)
